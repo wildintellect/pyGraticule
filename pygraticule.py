bee56575d7b0ca1c1100b8f6e342626438f379ee
Alex <tech@wildintellect.com>
2015-06-06 14:36:10 -0700

    Adjusting code into functions for better external usage

--- pygraticule.py
+++ pygraticule.py
@@ -17,128 +17,139 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+# The following double # section makes this script compatible with QGIS Processing Toolbox
+## minX = number -180
+## maxX = number 180
+## minY = number -90
+## maxX = number 90
+## outfile = file
+
+
 import sys, math
 import os, stat
 from optparse import OptionParser
 
-#TODO: Take bounding box as arg for setting range
- 
-parser = OptionParser(usage="""%prog [options]
-
-Generates a GeoJSON file with graticules spaced at specified interval.""")
-
-parser.add_option('-s', '--step_interval', dest='step_interval', default=1, type='int',
-                  help='Step interval in decimal degrees, defaults to 1.')
-
-parser.add_option('-o', dest='outfilename', default='',
-                  help='Output filename (with or without path), defaults to "graticule_1dd.geojson".')
-
-#TODO: Implement python OGR writing for multiple output format support.
-#parser.add_option('-p', '--shp', dest='shapefile', default=False, type='boolean",
-#                  help='Output a SHP file, defaults to False and requires ORG/GDAL.')
-
-
-(options, args) = parser.parse_args()
-
-
-#set the stepping of the increment, converting from string to interger
-#TODO: Calculate a node density based on the bounding box and interval defined by the user
-step = options.step_interval
-# destination file
-out_file = options.outfilename
-if out_file:
-    # remember the directory that file is contained by
-    out_dir = os.path.dirname( os.path.abspath(out_file) )
-    out_name = os.path.basename( os.path.abspath(out_file) )
-else:
-    out_dir = 'output/'
-    # destination file
-    out_extension = 'geojson'
-    # for the demo, we put the results in an "output dir for prettier results    
-    out_name = ('graticule_%ddd') % (step)
-    out_file = out_dir + out_name + '.' + out_extension
-
-# If the output directory doesn't exist, make it so we don't error later on file open()
-if not os.path.exists(out_dir):
-    print 'making dir...'
-    os.makedirs(out_dir)
-
-grid = open(out_file,"w")
-
-# Stub out the GeoJSON format wrapper
-header = ['{ "type": "FeatureCollection",','"features": [']
-footer = [']','}']
-
-grid.writelines(header)
+def make_graticule(minX=-180, minY=-90, maxX=180, maxY=90, step=1,outfile):
+    if outfile:
+        # remember the directory that file is contained by
+        outdir = os.path.dirname( os.path.abspath(outfile) )
+        outname = os.path.basename( os.path.abspath(outfile) )
+    else:
+        outdir = 'output/'
+        # destination file
+        outextension = 'geojson'
+        # for the demo, we put the results in an "output dir for prettier results    
+        outname = ('graticule_%ddd') % (step)
+        outfile = outdir + outname + '.' + outextension
     
-# Create Geojson lines horizontal, latitude
-for x in range(-90,91,step):
-    featstart = '''{ "type": "Feature",
-      "geometry": {
-        "type": "LineString",
-        "coordinates": ['''
-    grid.write(featstart)
-    for y in range(-180,181,1):
-        if y == -180:
-            grid.write("[")
-        else:
-            grid.write(",[")
-        #print y,x
-        grid.write(",".join([str(y),str(x)]))
-        grid.write("]")
-    # Figure out if it's North or South
-    if x >= 0:
-        direction = "N"
-    else:
-        direction = "S"
-    label = " ".join([str(abs(x)),direction])
-    featend = ''']},
-      "properties": {
-        "degrees": %d,
-        "direction": "%s",
-      "display":"%s",
-      "dd":%d,
-        }
-      },\n''' % (abs(x),direction,label,x)
-    grid.write(featend)
-
-# Create lines vertical
-for y in range(-180,181,step):
-    featstart = '''{ "type": "Feature",
-      "geometry": {
-        "type": "LineString",
-        "coordinates": ['''
-    grid.write(featstart)
-    for x in range(-90,91,1):
-        if x == -90:
-            grid.write("[")
-        else:
-            grid.write(",[")
-        #print y,x
-        grid.write(",".join([str(y),str(x)]))
-        grid.write("]")
+    # If the output directory doesn't exist, make it so we don't error later on file open()
+    if not os.path.exists(outdir):
+        print 'making dir...'
+        os.makedirs(outdir)
+    
+    grid = open(outfile,"w")
+    
+    # Stub out the GeoJSON format wrapper
+    header = ['{ "type": "FeatureCollection",','"features": [']
+    footer = [']','}']
+    
+    grid.writelines(header)
         
-    # Figure out if it's East or West
-    if y >= 0:
-        direction = "W"
-    else:
-        direction = "E"
-    label = " ".join([str(abs(y)),direction])
-    featend = ''']},
-      "properties": {
-        "degrees": %d,
-        "direction": "%s",
-      "display":"%s",
-      "dd":%d,
-        }
-      },\n''' % (abs(y),direction,label,y)
-    grid.write(featend)
-
-grid.writelines(footer)
-grid.close()
+    # Create Geojson lines horizontal, latitude
+    for x in range(minY,maxY+1,step):
+        featstart = '''{ "type": "Feature",
+          "geometry": {
+            "type": "LineString",
+            "coordinates": ['''
+        grid.write(featstart)
+        for y in range(minX,maxX+1,1):
+            if y == minX:
+                grid.write("[")
+            else:
+                grid.write(",[")
+            #print y,x
+            grid.write(",".join([str(y),str(x)]))
+            grid.write("]")
+        # Figure out if it's North or South
+        if x >= 0:
+            direction = "N"
+        else:
+            direction = "S"
+        label = " ".join([str(abs(x)),direction])
+        featend = ''']},
+          "properties": {
+            "degrees": %d,
+            "direction": "%s",
+          "display":"%s",
+          "dd":%d,
+            }
+          },\n''' % (abs(x),direction,label,x)
+        grid.write(featend)
+    
+    # Create lines vertical
+    for y in range(minX,maxX+1,step):
+        featstart = '''{ "type": "Feature",
+          "geometry": {
+            "type": "LineString",
+            "coordinates": ['''
+        grid.write(featstart)
+        for x in range(minY,maxY+1,1):
+            if x == minY:
+                grid.write("[")
+            else:
+                grid.write(",[")
+            #print y,x
+            grid.write(",".join([str(y),str(x)]))
+            grid.write("]")
+            
+        # Figure out if it's East or West
+        if y >= 0:
+            direction = "W"
+        else:
+            direction = "E"
+        label = " ".join([str(abs(y)),direction])
+        featend = ''']},
+          "properties": {
+            "degrees": %d,
+            "direction": "%s",
+          "display":"%s",
+          "dd":%d,
+            }
+          },\n''' % (abs(y),direction,label,y)
+        grid.write(featend)
+    
+    grid.writelines(footer)
+    grid.close()
 
 #if shapefile:
 #    try:
 #        ogr2ogr -f "ESRI Shapefile" out_file  out_file
 #    except:
 #        pass
+
+if __name__ == '__main__':
+    #TODO: Take bounding box as arg for setting range
+ 
+    parser = OptionParser(usage="""%prog [options]
+    
+    Generates a GeoJSON file with graticules spaced at specified interval.""")
+    
+    parser.add_option('-s', '--step_interval', dest='step_interval', default=1, type='int',
+                      help='Step interval in decimal degrees, defaults to 1.')
+    
+    parser.add_option('-o', dest='outfilename', default='',
+                      help='Output filename (with or without path), defaults to "graticule_1dd.geojson".')
+    
+    #TODO: Implement python OGR writing for multiple output format support.
+    #parser.add_option('-p', '--shp', dest='shapefile', default=False, type='boolean",
+    #                  help='Output a SHP file, defaults to False and requires ORG/GDAL.')
+    
+    
+    (options, args) = parser.parse_args()
+    
+    
+    #set the stepping of the increment, converting from string to interger
+    #TODO: Calculate a node density based on the bounding box and interval defined by the user
+    step = options.step_interval
+    # destination file
+    outfile = options.outfilename
